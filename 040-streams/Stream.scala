7d0f93fc89dfc9df0f172979a9fdb11458bb5bf4
Richard Banyi <rbanyi@me.com>
2017-04-09 15:19:51 +0200

    Redoing 4 after inproper saving

--- 040-streams/Stream.scala
+++ 040-streams/Stream.scala
@@ -8,21 +8,27 @@ package fpinscala.laziness
 import Stream._
 
 sealed trait Stream[+A] {
-
+  /*
   def headOption () :Option[A] =
     this match {
-      case Cons(h,t) => Some(h())
       case Empty => None
+      case Cons(h,t) => Some(h())
     }
+  */
+
+ // Exercise 4.7
+ def headOption() :Option[A] =
+   foldRight[Option[A]](None)((h, t) => Some(h))
+
 
   def tail :Stream[A] = this match {
-      case Cons(h,t) => t()
-      case Empty => Empty
+    case Empty => Empty
+    case Cons(h,t) => t()
   }
 
   def foldRight[B] (z : =>B) (f :(A, =>B) => B) :B = this match {
-      case Cons (h,t) => f (h(), t().foldRight (z) (f))
       case Empty => z
+      case Cons (h,t) => f (h(), t().foldRight (z) (f))
       // Note 1. f can return without forcing the tail
       // Note 2. this is not tail recursive (stack-safe) It uses a lot of stack
       // if f requires to go deeply into the stream. So folds sometimes may be
@@ -32,14 +38,14 @@ sealed trait Stream[+A] {
   // Note 1. eager; cannot be used to work with infinite streams. So foldRight
   // is more useful with streams (somewhat opposite to strict lists)
   def foldLeft[B] (z : =>B) (f :(A, =>B) =>B) :B = this match {
-      case Cons (h,t) => t().foldLeft (f (h(),z)) (f)
       case Empty => z
+      case Cons (h,t) => t().foldLeft (f (h(),z)) (f)
       // Note 2. even if f does not force z, foldLeft will continue to recurse
     }
 
   def exists (p : A => Boolean) :Boolean = this match {
-      case Cons (h,t) => p(h()) || t().exists (p)
       case Empty => false
+      case Cons (h,t) => p(h()) || t().exists (p)
       // Note 1. lazy; tail is never forced if satisfying element found this is
       // because || is non-strict
       // Note 2. this is also tail recursive (because of the special semantics
@@ -69,6 +75,40 @@ sealed trait Stream[+A] {
     case _ => empty
   }
   //def find (p :A => Boolean) :Option[A] = this.filter (p).headOption
+
+  // Exercise 4.5
+  def forAll(p: A => Boolean): Boolean = {
+    foldRight(true)((h, t) => p(h) && t)
+  }
+
+  // Exercise 4.6
+  def takeWhile2(p: A => Boolean): Stream[A] =
+    foldRight[Stream[A]](empty)((a, b) =>
+        if (p(a)) cons(a, b)
+        else empty)
+
+  // Exercise 4.8
+  def map[B] (f: A => B): Stream[B] =
+    foldRight[Stream[B]](empty)((h, t) => cons(f(h), t))
+
+  def filter(p: A => Boolean): Stream[A] =
+    foldRight[Stream[A]](empty)((h, t) => if (p(h)) cons(h, t) else t)
+
+  def append[B >: A](that: Stream[B]): Stream[B] =
+    foldRight(that)((h, t) => cons(h, t))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight[Stream[B]](empty)((h, t) => f(h).append(t))
+
+  // Exercise 4.9
+  //def find(p: A => Boolean): Option[A] =
+  //  this.filter(p).headOption
+
+  // Exercise 4.11
+
+  // Exercise 4.12
+
+  // Exercise 4.13
 }
 
 
@@ -111,5 +151,11 @@ object Stream {
     cons(n, from(n+1))
   }
 
+  // Exercise 4.10
+  val fibs = {
+    def go(f0: Int, f1: Int): Stream[Int] = 
+      cons(f0, go(f1, f0+f1))
+    go(0,1)
+  }
+
 }
-// vim:tw=0:cc=80:nowrap
