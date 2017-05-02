3ec167eea596bf0704012bfbaab3858071719ae8
Richard Banyi <rbanyi@me.com>
2017-05-02 11:11:44 +0200

    Tokenized Vector

--- 110-sentiment-analysis/src/main/scala/Main.scala
+++ 110-sentiment-analysis/src/main/scala/Main.scala
@@ -6,12 +6,19 @@ import org.apache.spark.ml.feature.Tokenizer
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
+import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer}
 
+import scala.collection.mutable.WrappedArray
+import org.apache.spark.sql.functions
+import org.apache.spark.sql
 
 object Main {
 
 	type Embedding       = (String, List[Double])
 	type ParsedReview    = (Integer, String, Double)
+  type TokenizedReview = (Integer, Double, WrappedArray[String])
+  type TokenizedVector = (Integer, Double, List[List[Double]])
+  type ExplodeReview   = (Integer, Double, String)
 
 	org.apache.log4j.Logger getLogger "org"  setLevel (org.apache.log4j.Level.WARN)
 	org.apache.log4j.Logger getLogger "akka" setLevel (org.apache.log4j.Level.WARN)
@@ -55,16 +62,49 @@ object Main {
 			.withColumnRenamed ("_2", "vec")
 			.as[Embedding]
 
+  def tokenize (reviews: Dataset[ParsedReview]): Dataset[TokenizedReview] =
+    new Tokenizer()
+      .setInputCol("text")
+      .setOutputCol("words")
+      .transform(reviews)
+      .drop("text")
+      .as[TokenizedReview]
+
+  def explodeReview (tokens: Dataset[TokenizedReview]): Dataset[ExplodeReview] =
+    tokens.flatMap(
+      r => {
+        val (id, overall, words) = r
+        words.map( w => (id, overall, w))
+      }
+      )
+      .withColumnRenamed("_1", "id")
+      .withColumnRenamed("_2", "overall")
+      .withColumnRenamed("_3", "word")
+      .as[ExplodeReview]
+
+  def tokenizeVector (tokens: Dataset[ExplodeReview],
+    glove: Dataset[Embedding]): Dataset[TokenizedVector] =
+      tokens.join(glove, "word")
+        .groupBy("id", "overall")
+        .avg("vec")
+        .drop("word")
+
   def main(args: Array[String]) = {
 
-    val glove  = loadGlove ("path/to/glove/file") // FIXME
-    val reviews = loadReviews ("path/to/amazon/reviews/file") // FIXME
+    val glove  = loadGlove ("/Users/richardbanyi/Developer/itu/2017-adpro/110-sentiment-analysis/data/glove/glove.6B.300d.txt") // FIXME
+    val reviews = loadReviews ("/Users/richardbanyi/Developer/itu/2017-adpro/110-sentiment-analysis/data/Musical_Instruments_5.json") // FIXME
 
     // replace the following with the project code
     glove.show
     reviews.show
 
-		spark.stop
+    val tokenized = tokenize (reviews)
+    tokenized.show
+
+    val explode = explodeReview(tokenized)
+    explode.show
+
+    spark.stop
   }
 
 }
