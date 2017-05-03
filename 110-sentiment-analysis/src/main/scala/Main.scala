3fdb37274e10f427468d08b88d2337410448da8a
Richard Banyi <rbanyi@me.com>
2017-05-03 21:37:21 +0200

    Create tests

--- 110-sentiment-analysis/src/main/scala/Main.scala
+++ 110-sentiment-analysis/src/main/scala/Main.scala
@@ -2,23 +2,18 @@
 // To execute this example, run "sbt run" or "sbt test" in the root dir of the project
 // Spark needs not to be installed (sbt takes care of it)
 
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.Tokenizer
-import org.apache.spark.sql.Dataset
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.types._
-import org.apache.spark.ml.feature.{RegexTokenizer, Tokenizer}
-
-import scala.collection.mutable.WrappedArray
-import org.apache.spark.sql.functions
-import org.apache.spark.sql
+import org.apache.spark.sql.functions._
 
 object Main {
 
 	type Embedding       = (String, List[Double])
 	type ParsedReview    = (Integer, String, Double)
-  type TokenizedReview = (Integer, Double, WrappedArray[String])
-  type TokenizedVector = (Integer, Double, List[List[Double]])
-  type ExplodeReview   = (Integer, Double, String)
 
 	org.apache.log4j.Logger getLogger "org"  setLevel (org.apache.log4j.Level.WARN)
 	org.apache.log4j.Logger getLogger "akka" setLevel (org.apache.log4j.Level.WARN)
@@ -27,15 +22,14 @@ object Main {
 		.master  ("local[9]")
 		.getOrCreate
 
-  import spark.implicits._
+	import spark.implicits._
 
 	val reviewSchema = StructType(Array(
 			StructField ("reviewText", StringType, nullable=false),
 			StructField ("overall",    DoubleType, nullable=false),
 			StructField ("summary",    StringType, nullable=false)))
 
-	// Read file and merge the text abd summary into a single text column
-
+	// Read file and merge the text and summary into a single text column
 	def loadReviews (path: String): Dataset[ParsedReview] =
 		spark
 			.read
@@ -50,61 +44,106 @@ object Main {
 			.withColumnRenamed ("_3", "overall")
 			.as[ParsedReview]
 
-  // Load the GLoVe embeddings file
-
-  def loadGlove (path: String): Dataset[Embedding] =
+	// Load the GLoVe embeddings file
+	def loadGlove (path: String): Dataset[Embedding] =
 		spark
 			.read
 			.text (path)
-      .map  { _ getString 0 split " " }
-      .map  (r => (r.head, r.tail.toList.map (_.toDouble))) // yuck!
-			.withColumnRenamed ("_1", "word" )
+	  		.map  { _ getString 0 split " " }
+	  		.map  (r => (r.head, r.tail.toList.map (_.toDouble))) // yuck!
+			.withColumnRenamed ("_1", "word")
 			.withColumnRenamed ("_2", "vec")
 			.as[Embedding]
 
-  def tokenize (reviews: Dataset[ParsedReview]): Dataset[TokenizedReview] =
-    new Tokenizer()
-      .setInputCol("text")
-      .setOutputCol("words")
-      .transform(reviews)
-      .drop("text")
-      .as[TokenizedReview]
+	// Split the review in words
+	def tokenizer (reviews: Dataset[ParsedReview]) =
+		new Tokenizer().setInputCol("text").setOutputCol("words")
+									 .transform(reviews).as[(Integer, String, Double, Array[String])]
 
-  def explodeReview (tokens: Dataset[TokenizedReview]): Dataset[ExplodeReview] =
-    tokens.flatMap(
-      r => {
-        val (id, overall, words) = r
-        words.map( w => (id, overall, w))
-      }
-      )
-      .withColumnRenamed("_1", "id")
-      .withColumnRenamed("_2", "overall")
-      .withColumnRenamed("_3", "word")
-      .as[ExplodeReview]
+	def wordsSeperator (tokens: Dataset[(Integer, String, Double, Array[String])]) =
+		tokens.flatMap { case (id, text, overall, words) =>
+				words.map ( 
+					word => ( id, if (overall > 3.0) 2.0 else if (overall == 3.0) 1.0 else 0.0, word ) 
+		) }.withColumnRenamed("_3", "word")
 
-  def tokenizeVector (tokens: Dataset[ExplodeReview],
-    glove: Dataset[Embedding]): Dataset[TokenizedVector] =
-      tokens.join(glove, "word")
-        .groupBy("id", "overall")
-        .avg("vec")
-        .drop("word")
+	def features (words: Dataset[Row], glove: Dataset[Embedding]) = (
+		   words.join(glove, words("word") === glove("word")).drop("word")
+			.withColumn("count", lit(1))
+		  .as[(Integer, Double, Array[Double], Integer)])
+			.groupByKey(_._1)
+			// Sum vectors
+			.reduceGroups(
+					(x, y) => (x._1, x._2,
+										 x._3.zip(y._3).map(z => (z._1 + z._2)), 
+										 x._4 + y._4))
+		  .map(_._2)
+		  // Average vectors
+		  .map(x => (x._1, x._2, Vectors.dense(x._3.map( d => d / x._4 ))))
+		  .withColumnRenamed("_1", "id")
+		  .withColumnRenamed("_2", "label")
+		  .withColumnRenamed("_3", "features")
 
-  def main(args: Array[String]) = {
+	def perceptron (n: Int, splits: Array[Dataset[Row]], maxIter: Int) : Double = 
+	{
+		val layers = Array[Int](50, 5, 4, 3)
+		val trainer = new MultilayerPerceptronClassifier()
+			.setLayers (layers)
+			.setBlockSize (128)
+			.setSeed (1234L)
+			.setMaxIter (maxIter)
+		val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
 
-    val glove  = loadGlove ("/Users/richardbanyi/Developer/itu/2017-adpro/110-sentiment-analysis/data/glove/glove.6B.300d.txt") // FIXME
-    val reviews = loadReviews ("/Users/richardbanyi/Developer/itu/2017-adpro/110-sentiment-analysis/data/Musical_Instruments_5.json") // FIXME
+		val first = if (n == 0) 1 else 0
+		val train = (first+1 to 9).foldLeft (splits(first)) (
+			(bcc, m) => {
+				if (m != n) bcc.union (splits(m))
+				else bcc
+				}
+			)
+		val test = splits(n)
+		val model = trainer.fit(train)
+		val result = model.transform(test)
 
-    // replace the following with the project code
-    glove.show
-    reviews.show
+		evaluator.evaluate (result.select("prediction", "label"))
+	}
 
-    val tokenized = tokenize (reviews)
-    tokenized.show
+	def accuracy (glove: Dataset[Embedding], reviews: Dataset[ParsedReview]) = {
+		// Seperate all the words from the reviews
+		//val tokenized = tokenizer(reviews)
+    //tokenized.show
+    //println(tokenized.take(1).head._4.length)
 
-    val explode = explodeReview(tokenized)
-    explode.show
+    val words = wordsSeperator(tokenizer(reviews))
+    //words.groupBy("_1").count().filter($"_1" === 0).show
+    // Get the score for each word
+		val average = features(words, glove)
+    //println(average.collect()(0))
+    //average.printSchema()
+    // average.printSchema()
+		/  Multilayer perceptron classifier
+		val splits = average.randomSplit (
+			Array(.1, .1, .1, .1, .1, .1, .1, .1, .1, .1), seed = 1234L)
 
-    spark.stop
+		(0 to 9).foldLeft (List[Double]()) (
+			(acc, n) => { perceptron(n, splits, 8/*Max iterations*/) :: acc })
   }
 
+	def run () = {
+		val glove  = loadGlove ("data/glove/glove.6B.50d.txt")
+		val reviews = loadReviews ("data/Musical_Instruments_5_small.json")		
+
+    accuracy(glove, reviews)
+		/* val accuracyList = accuracy(glove, reviews)
+		println ("")
+		println ("###########################################")
+		println ("### AVERAGE ACCURACY: " + accuracyList.foldLeft(0.0)(_+_) + " ###")
+		println ("###########################################")
+		println ("")
+    */
+		spark.stop
+	}
+
+	def main(args: Array[String]) = run()
+
 }
+
--- 110-sentiment-analysis/src/test/scala/SentimentSpec.scala
+++ 110-sentiment-analysis/src/test/scala/SentimentSpec.scala
@@ -1,6 +1,7 @@
 import org.scalatest.{FreeSpec, Matchers, BeforeAndAfterAll}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Dataset
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
 class SentimentSpec extends FreeSpec with Matchers with BeforeAndAfterAll {
 
@@ -16,14 +17,35 @@ class SentimentSpec extends FreeSpec with Matchers with BeforeAndAfterAll {
 
   import spark.implicits._
 
+  val glove  = Main.loadGlove ("data/glove/glove.6B.50d.txt")
 
-  val glove  = Main.loadGlove ("path/to/glove/file/in/your/filesystem")
-
-  "put your tests here"   - {
+   "put your tests here" - {
 
     "nested tests group" - {
-      "individual test #1" in { ??? }
-      "individual test #2" in { ??? }
+      "individual test #1" in {
+        val reviews = Main.loadReviews ("data/Musical_Instruments_5_small.json")
+        val words = Main.wordsSeperator(Main.tokenizer(reviews))
+        assert(Main.features(words, glove).collect()(0).getDouble(1) == 2.0) // Negative
+        assert(Main.features(words, glove).collect()(1).getDouble(1) == 2.0) // Positive
+      }
+      "individual test #2" in {
+        val reviews = Main.loadReviews ("data/Musical_Instruments_5_small.json")
+        val tokenized = Main.tokenizer(reviews).take(1).head._4.length
+        val words = Main.wordsSeperator(Main.tokenizer(reviews))
+        val word = words.groupBy("_1").count().filter($"_1" === 0).head.getLong(1)
+        assert(tokenized == word)
+      }
+      "individual test #3" in {
+        val reviews = Main.loadReviews ("data/Musical_Instruments_5_small.json")
+        val glove = Main.loadGlove ("data/glove/glove.6B.50d.txt")
+        val words = Main.wordsSeperator(Main.tokenizer(reviews))
+        val average = Main.features(words, glove)
+        val expected_features = Array((31, 2.0, Vectors.dense(0.1255215,0.28322693,-0.09646450000000001,0.1790677,0.328579,0.14379475000000003,-0.4609889000000001,-0.07140223999999999,0.04593803849999999,0.027005149999999995,0.031075499999999978,0.029017599999999998,-0.2824075,0.11188825000000002,0.277803425,0.25963250000000004,0.03502365,0.1528346,-0.364208455,-0.5585006,0.18188120000000002,0.26288645000000005,0.10764129999999998,-0.005248850000000008,0.20335835000000002,-1.495238,-0.3598839,0.24952902999999999,0.45840395,-0.17822079999999998,3.4493094999999996,0.24172725000000006,-0.11393490000000002,-0.20979199999999998,0.05814505649999999,0.006615165000000011,0.13158271500000002,0.2697706,0.1617977,-0.20023204999999997,0.029955849999999985,0.11593748,-0.12488065,0.28972565,-0.07222349999999998,0.07652325,-0.018566174999999997,0.15861755000000005,0.02197699999999999,0.21965479999999998)))
+        val test_row = average.take(1)
+        assert(test_row === expected_features)
+      }
+      "individual test #4" in { assert(true) }
+      "individual test #5" in { assert(true) }
     }
 
   }
