d5ed6fb85f8c2ddfee08cc505d3bb12a0a5d22f9
Richard Banyi <rbanyi@me.com>
2017-07-10 19:11:53 +0200

    Property Testing

--- 080-mini-project-1/src/test/scala/fpinscala/laziness/StreamSpecWasowski.scala
+++ 080-mini-project-1/src/test/scala/fpinscala/laziness/StreamSpecWasowski.scala
@@ -39,16 +39,95 @@ class StreamSpecWasowski extends FlatSpec with Checkers {
     for { la <- arbitrary[List[A]] suchThat (_.nonEmpty)}
     yield list2stream (la)
 
+
+  val smallInt = Gen.choose(0, 10000)
+
   // a property test:
 
   it should "return the head of the stream packaged in Some (02)" in check {
+
     // the implict makes the generator available in the context
     implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
     ("singleton" |:
       Prop.forAll { (n :Int) => cons (n,empty).headOption == Some (n) } ) &&
     ("random" |:
-      Prop.forAll { (s :Stream[Int]) => s.headOption != None } )
+      Prop.forAll { (s :Stream[Int]) => s.headOption != None } ) &&
+    ("tail enforcing" |:
+      Prop.forAll { (n: Int) => { cons(n, cons(throw new Error("Laziness tests"), Empty)).headOption; true } } )
+  }
 
+  behavior of "take"
+
+  it should "take should not force any tails of the Stream it manipulates" in check {
+    cons(9, cons(throw new Error("Laziness tests"), Empty)).take(1); true
+  }
+
+  it should "take should not force any heads of the Stream it manipulates" in check {
+    cons(throw new Error("Laziness"), cons(throw new Error("Laziness tests"), Empty)).take(2); true
+  }
+
+  it should "take(n) does not force (n+1)st head ever (even if we force all elements of take(n))" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    Prop.forAll { (s: Stream[Int]) => {
+      val n = s.toList.length
+      s.append(cons(throw new Error("Laziness tests"), Empty)).take(n)
+      true
+    } }
+  }
+
+  it should "s.take(n).take(n) == s.take(n) for any Stream s and any (idempotency)" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    Prop.forAll { (s: Stream[Int], n: Int) => {
+      s.take(n).take(n).toList == s.take(n).toList
+    } }
+  }
+
+  behavior of "drop"
+
+  it should "s.drop(n).drop(m) == s.drop(n+m) for any n, m (additivity)" in check {
+    Prop.forAll (genNonEmptyStream[Int], smallInt, smallInt)
+    { (s: Stream[Int], n: Int, m: Int) => {
+      s.drop(n).drop(m).toList == s.drop(n+m).toList
+    } }
+  }
+
+  it should "s.drop(n) does not force any of the dropped elements heads" in check {
+    cons(throw new Error("Laziness"), cons(throw new Error("Laziness tests"), Empty)).drop(1); true
+  }
+
+  it should "the above should hold even if we force some stuff in the tail" in check {
+    cons(9, cons(throw new Error("Laziness tests"), Empty)).drop(1); true
+  }
+
+  behavior of "map"
+
+  it should "x.map(id) == x (where id is the identity function)" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    Prop.forAll { (s: Stream[Int]) => {
+      s.map(identity).toList == s.toList
+    } }
+  }
+
+  it should "map terminates on infinite streams" in check {
+    Prop.forAll { (n: Int) => { Stream.from(n).map(identity); true } }
+  }
+
+  behavior of "append"
+
+  it should "it should not force tail or head of the appended stream" in check {
+    Stream(1, 2, 3, 4).append(cons(throw new Error("Laziness tests"), Empty))
+    true
+  }
+
+  it should "it should not change when append empty" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    Prop.forAll { (s: Stream[Int]) => {
+      s.append(Empty).toList == s.toList
+    } }
+  }
+
+  it should "append terminates on infinite streams" in check {
+    Prop.forAll { (n: Int) => { Stream.from(n).append(Stream.from(10)); true } }
   }
 
 }
