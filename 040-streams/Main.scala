3434dffc2f681ebe60d174daa2ef7a3ea9fa65fd
Richard Banyi <rbanyi@me.com>
2017-08-05 21:38:22 +0200

    Streams Done

--- 040-streams/Main.scala
+++ 040-streams/Main.scala
@@ -17,4 +17,20 @@ println (l1.headOption)
 println (l2.headOption)
 println (l3.headOption)
 
-
+val naturals: Stream[Int] = from(1)
+assert(Stream.to(3).toList == List(1,2,3))
+println (naturals.take(1000000000).drop(41).take(10).toList)
+assert( naturals.take(1000000000).drop(41).take(10).toList == List(42, 43, 44, 45, 46, 47, 48, 49, 50, 51))
+println (naturals.takeWhile(_<1000000000).drop(100).take(50).toList)
+println (naturals.forAll (_ < 0))
+println (naturals.forAll2 (_ < 0))
+// assert (naturals.forAll2 (_ > 0) == true)
+println (naturals.takeWhile2(_<100000000).drop(100).take(50).toList)
+println (l1.headOption2)
+println (l2.headOption2)
+println (l3.headOption2)
+naturals.map (_*2).drop (30).take (50).toList
+naturals.drop(42).filter (_%2 ==0).take (30).toList
+println (fibs.take(100).toList)
+fibViaUnfold.take(100).toList ==fibs.take(100).toList
+from(1).take(1000000000).drop (41).take(10).toList == fromViaUnfold(1).take(1000000000).drop (41).take(10).toList
--- 040-streams/Stream.scala
+++ 040-streams/Stream.scala
@@ -46,7 +46,64 @@ sealed trait Stream[+A] {
       // of ||)
     }
 
-  //def find (p :A => Boolean) :Option[A] = this.filter (p).headOption
+  // Exercise 4.2
+  def toList :List[A] = this match {
+    case Cons(h,t) => h() :: t().toList
+    case Empty => Nil
+  }
+
+  // Exercise 4.3
+  def take (n: Int) :Stream[A] = this match {
+    case Cons(h,t) if (n > 0) => cons(h(), t().take(n-1))
+    case _ => empty
+  }
+
+  def drop (n: Int): Stream[A] = this match {
+    case Cons(_,t) if (n > 0) => t().drop(n-1)
+    case _ => this
+  }
+
+  // Exercise 4.4
+  def takeWhile (p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) if p(h()) => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
+
+  // Exercise 4.5
+  def forAll (p: A => Boolean) :Boolean = this match {
+    case Cons(h,t) if p(h()) => t().forAll(p)
+    case _ => false
+  }
+
+  def forAll2 (p: A => Boolean) :Boolean =
+    foldRight(true)((h,t) => p(h) && t)
+
+  // Exercise 4.6
+  def takeWhile2 (p: A => Boolean) :Stream[A] =
+    foldRight[Stream[A]](empty)((h,t) => if (p(h)) cons(h,t) else empty)
+
+  // Exercise 4.7
+  def headOption2: Option[A] =
+    foldRight[Option[A]](None)((h,t) => Some(h))
+
+  // Exercise 4.8
+  def map[B] (p: A => B) :Stream[B] =
+    foldRight[Stream[B]](empty)((h,t) => cons(p(h),t))
+
+  def filter (p: A => Boolean) :Stream[A] =
+    foldRight[Stream[A]](empty)((h,t) => if (p(h)) cons(h,t) else t)
+  
+  def append[B >: A] (that: Stream[B]) :Stream[B] =
+    foldRight(that)((h,t) => cons(h, t))
+
+  def flatMap[B] (p: A => Stream[B]) :Stream[B] =
+    foldRight[Stream[B]](empty)((h,t) => p(h).append(t))
+
+  // Exercise 4.9
+  def find (p :A => Boolean) :Option[A] = this.filter (p).headOption
+
+  // Exercise 4.9
+  // def mapViaUnfold[B] (p: A => B): Stream[B] 
 }
 
 
@@ -72,6 +129,40 @@ object Stream {
     // Note 1: ":_*" tells Scala to treat a list as multiple params
     // Note 2: pattern matching with :: does not seem to work with Seq, so we
     //         use a generic function API of Seq
+
+  // Exercise 4.11
+  def unfold[A,S] (z: S) (f: S => Option[(A,S)]): Stream[A] = f(z) match {
+    case Some((a,s)) => cons(a, unfold(s)(f))
+    case None => empty
+  }
+
+  // Exercise 4.1
+  def to (n: Int) :Stream[Int] = {
+    def reverse(acc: Int) :Stream[Int] = {
+      if (acc <= n) cons(acc, reverse(acc+1))
+      else empty
+    }
+    reverse(1)
+  }
+
+  def from (n: Int) :Stream[Int] = {
+    cons(n, from(n+1))
+  }
+
+  // Exercise 4.10
+  def fibs: Stream[Int] = {
+    def go(n: Int, nn: Int) :Stream[Int] = {
+      cons(n, go(nn, n+nn))
+    }
+    go(0,1)
+  }
+
+  // Exercise 4.12
+  def fibViaUnfold: Stream[Int] = 
+    unfold((0,1)){ case (a,b) => Some(a, (b, a+b)) }
+
+  def fromViaUnfold (n: Int): Stream[Int] = 
+    unfold(n)(n => Some(n, n+1))
 }
 
 // vim:tw=0:cc=80:nowrap
